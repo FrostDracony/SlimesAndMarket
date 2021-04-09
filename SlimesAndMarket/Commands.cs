39294f6a7c8d245f38e774cdde4d1b55b65255b3
Your Name <60100684+FrostDracony@users.noreply.github.com>
2021-04-09 12:48:12 +0200

    Update: * Repaired some errors * Adapted it for all Vanilla slimes * Lowered the price of Saber Slimes and Gold Slimes * Added compability with other mods.
    If any other mod wants to use my mod to add custom things to the market, all what they have to do is:
    
    - in "modinfo.json" add this (at best at the end, the order dosen't count but that's in order to read better):
    
    "load_before": [ "slimesandmarket" ]
    
    - in your main class in "Load":
    
    if(SRModLoader.IsModPresent("slimesandmarket"))
                    {
                        SlimesAndMarket.ModdedSellable.moddedItemsToSell.Add(new SlimesAndMarket.ModdedSellable());
                    }
    
    First argumeter: the id of your mod
    Second argumeter: the price
    Third argumeter: the saturation
    Fourth argumeter: the item itself to be sold
    The final parameter; if you want drones to suck up your slime or notUpdate 1.1:
    * Fixed a strange bug/error in the previous version
    *Slimes can now be taken by drones
    *I rised the prise for Saber, Gold and Quicksilver Slimes

---
+++ SlimesAndMarket/Commands.cs
@@ -0,0 +1,48 @@
+﻿using SRML.Console;
+using System.Collections.Generic;
+
+namespace SlimesAndMarket
+{
+    class RegistredModsCommand : ConsoleCommand
+    {
+        // The command's command ID; how someone using the console will call your command.
+        public override string ID => "slimes_and_market_registred_mods";
+
+        // What will appear when the command is used incorrectly or viewed via the help command.
+        // Remember, <> is a required argument while [] is an optional one.
+        public override string Usage => "slimes_and_market_registred_mods";
+
+        // A description of the command that will appear when using the help command.
+        public override string Description => "Reveals what mods are registred and what not (what mods uses this mod to make their things sellable)";
+
+        // The code that the command runs. Requires you to return a bool.
+        public override bool Execute(string[] args)
+        {
+            // Checks if the code has enough arguments.
+            if (args == null || args.Length > 0)
+            {
+                Console.LogError("Incorrect amount of arguments!", true);
+                return false;
+            }
+
+            Console.Log("Mods registred are:");
+
+            foreach(string mod in ModdedThings.modsRegistred)
+            {
+                Console.Log(mod);
+            }
+
+            return true;
+        }
+
+        // A list that the autocomplete references from. You must return a List<string>.
+        public override List<string> GetAutoComplete(int argIndex, string argText)
+        {
+            // Checks which argument you're on.
+            List<string> result;
+            result = base.GetAutoComplete(argIndex, argText);
+
+            return result;
+        }
+    }
+}
--- SlimesAndMarket/Main.cs
+++ SlimesAndMarket/Main.cs
@@ -1,64 +1,237 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using SRML;
 using SRML.SR;
 
+using LargoLibrary;
+
+using Console = SRML.Console.Console;
+
 namespace SlimesAndMarket
 {
-    public static class Extension {
-        public static EconomyDirector.ValueMap FindById(this Array obj, Identifiable.Id id, bool plort) //This is an extension (it's called so in C#) for arrays (EconomyDirector.ValueMap inherits from the class Array). Continue below
-            //Parameter 1: The target type for the extension and our array
-            //Parameter 2: The id of the slime or the plort
-            //Parameter 3: It tells us if Parameter 2 is the id of a slime or of a plort
-        {
-            foreach (EconomyDirector.ValueMap valueMap in obj) //For each vanilla sellable plorts
-            {
-                if (valueMap.accept.id == (plort ? id : SRSingleton<GameContext>.Instance.SlimeDefinitions.GetSlimeByIdentifiableId(id).Diet.Produces[0])) //Ternary Operator: If Parameter 3, "boolean plort", [Continue below]
-                    //is true, then just make the control with Parameter 2 (it is a plort), else get the plort of the slime (use Parameter 2 as the id of the current slime)
-                {
-                    return valueMap; //If we found the matching slime then return th correct valueMap (informations about the plort that you normally sell)
-                }
-            }
-            return null; //If nothing was found then return null and there will be an error in the mod
-        }
-    }
-
-    public class Main : ModEntryPoint
+    public static class Extension
     {
-        public override void PostLoad()
+        static List<Identifiable.Id> registredSlimes = new List<Identifiable.Id>(); //A List used to mark what slimes already got registred and what not
+        static List<Identifiable.Id> largoSlimes = LargoLibrary.LargoGenerator.generatedIds;
+
+        public static bool FindContent(this Array obj, Identifiable.Id comparator, string print = "") //This is an extension (it's called so in C#) for arrays that allows me to search if something is in that array or not (without erroring).
         {
-            EconomyDirector.ValueMap[] valueMaps = SRSingleton<SceneContext>.Instance.EconomyDirector.baseValueMap; //Everything that can be sold
-            Identifiable.Id idPlort = Identifiable.Id.NONE; //The plort used for puddle/fire slimes
-
-            try //Try the code there, if some error arises then it calls the "catch" function 
-            {
-                foreach (Identifiable.Id slime in Identifiable.SLIME_CLASS) //For each slime int he slime class
+            if (print != "")
+                //Console.Log("Printing Array: " + print);
+                foreach (Identifiable.Id content in obj) //For each element in our array
                 {
-                    bool puddleFlag = (slime == Identifiable.Id.PUDDLE_SLIME) ? true : false; //Ternary Operator: If the id is the id of a puddle slime then return true, else false
-                    bool fireFlag = (slime == Identifiable.Id.FIRE_SLIME) ? true : false; //Same as above, only with fire slimes
-                    bool flag = puddleFlag || fireFlag; //If the slime is a puddle slime or a fire slime, the this variable will be true, else they will be false
-                    if (flag)
-                        idPlort = (puddleFlag) ? Identifiable.Id.PUDDLE_PLORT : Identifiable.Id.FIRE_PLORT; // If the id (the slime variable) is a puddle or fire slime then assign the variable "idPlort" the puddle/fire plort id
+                    if (print != "")
+                        //Console.Log("Printing Id: " + content);
+                        if (content == comparator) //If the ID is there
+                        {
+                            //Console.Log("   Something got found in the comparator: " + content);
+                            return true; //Then return true
+                        }
+                }
+            //Console.Log("   Nothing got found in the comparator");
+            return false; //Else return false
+        }
 
-                    if (slime != Identifiable.Id.TARR_SLIME && slime != Identifiable.Id.LUCKY_SLIME) //Avoid the code for tarrs and lucky slimes (they dont have plorts in vanilla and currently this mod dosen't support modded slimes)
+        public static bool IsNullOrEmpty<T>(this T[] array) //To see if an array exists and/or has 
+        {
+            if (array == null || array.Length == 0)
+                return true;
+            else
+                return array.All(item => item == null);
+        }
+
+        public static EconomyDirector.ValueMap FindById(this Array obj, string puddleSlime)
+        {
+            Identifiable.Id acceptId = Identifiable.Id.NONE;
+            Identifiable.Id acceptIdPlort = Identifiable.Id.NONE;
+
+            foreach (EconomyDirector.ValueMap valueMap in obj)
+            {
+                if (puddleSlime == "PUDDLE_SLIME")
+                {
+                    acceptId = Identifiable.Id.PUDDLE_SLIME;
+                    acceptIdPlort = Identifiable.Id.PUDDLE_PLORT;
+                }
+                else
+                {
+                    acceptId = Identifiable.Id.FIRE_SLIME;
+                    acceptIdPlort = Identifiable.Id.FIRE_PLORT;
+                }
+
+                if (!registredSlimes.ToArray().FindContent(acceptId))
+                { //If the slime wasn't registred before, then continue, else dont continue
+
+                    if (valueMap.accept.id == acceptIdPlort) //If the Identifiable.Id matches with the plort already registred
                     {
-                        EconomyDirector.ValueMap valueMap = (flag) ? valueMaps.FindById(idPlort, true) : valueMaps.FindById(slime, false); //Read below
-                        //If the slime is a puddle/fire slime then add the plort id, else just add the id of the slime (true/false argumeter tells the programm if it's a puddle/fire slime or regular slimes, except tarrs and lucky slimes)
-                        //We are getting the informations of the plort of the correct slime (to make normal prices)
-                        PlortRegistry.RegisterPlort(slime, valueMap.value * 4, valueMap.fullSaturation); //Then register the slime (make so that you can sell it) for 4 times the original price more.
-
+                        registredSlimes.Add(acceptId); //Add this slime to the eatmap: now it's registred, and it cannot be registred again
+                        return valueMap; //Then we found a matching slime, so return the correct valueMap (informations about the plort that you normally sell)
                     }
                 }
             }
-            catch (Exception e) //But if an error was found
-            {
-                SRML.Console.Console.Log("Sellable Slimes Mod error: " + e.Message); //Then log the error into the SRML.log file
-            }
+            return null;
         }
 
+        public static EconomyDirector.ValueMap FindById(this Array obj, Identifiable.Id id)
+
+        {
+            foreach (EconomyDirector.ValueMap valueMap in obj) //For each vanilla sellable plorts
+            {
+                bool check = true;
+                Identifiable.Id acceptId = Identifiable.Id.NONE;
+
+                if (check)
+                {
+                    if (SRSingleton<GameContext>.Instance.SlimeDefinitions.GetSlimeByIdentifiableId(id).Diet.Produces.IsNullOrEmpty()) //If the "plort" parameter is not a slime and the slime produces no plorts, avoid the error and just pass on the next slime
+                        break;
+                }
+
+                if (check)
+                    acceptId = SRSingleton<GameContext>.Instance.SlimeDefinitions.GetSlimeByIdentifiableId(id).Diet.Produces[0];
+
+                //Identifiable.Id acceptId = (plort ? id : SRSingleton<GameContext>.Instance.SlimeDefinitions.GetSlimeByIdentifiableId(id).Diet.Produces[0]); //Ternary Operator: If Parameter 3, "boolean plort", [Continue below]
+                //is true, then just make the control with Parameter 2 (it is a plort), else get the plort of the slime (use Parameter 2 as the id of the current slime)
+
+                if (!registredSlimes.ToArray().FindContent(acceptId))
+                { //If the slime wasn't registred before, then continue, else dont continue
+
+                    if (valueMap.accept.id == acceptId) //If the Identifiable.Id matches with the plort already registred
+                    {
+                        registredSlimes.Add(acceptId); //Add this slime to the eatmap: now it's registred, and it cannot be registred again
+                        return valueMap; //Then we found a matching slime, so return the correct valueMap (informations about the plort that you normally sell)
+                    }
+                }
+            }
+
+            return null; //If nothing was found then return null
+        }
+
+        
+
+        public class Main : ModEntryPoint
+        {
+
+            public static bool IsModLoaded(string modId)
+            {
+                return SRModLoader.IsModPresent(modId);
+            }
+
+            public override void PreLoad()
+            {
+                HarmonyInstance.PatchAll();
+                SRML.Console.Console.RegisterCommand(new RegistredModsCommand());
+            }
+
+            public override void Load()
+            {
+                EconomyDirector.ValueMap[] valueMaps = SRSingleton<SceneContext>.Instance.EconomyDirector.baseValueMap; //Everything that can be sold
+                Identifiable.Id idPlort = Identifiable.Id.NONE; //The plort used for puddle/fire slimes
+                try //Try the code there, if some error arises then it calls the "catch" function 
+                {
+                    foreach (Identifiable.Id slime in ModdedThings.VANILLA_SLIMES) //For each slime int he slime class
+                    {
+                        /*if (!slime.ToString().Contains(Identifiable.SLIME_SUFFIX) || slime.ToString() == "GLITCH_SLIME" || Identifiable.TARR_CLASS.Contains(slime) || slime.ToString().Split('_').Length > 2)
+                            continue;*/
+
+                        Console.Log("Printing Slime: " + slime.ToString());
+
+                        /*bool puddleFlag = (slime == Identifiable.Id.PUDDLE_SLIME) ? true : false; //Ternary Operator: If the id is the id of a puddle slime then return true, else false
+                        bool fireFlag = (slime == Identifiable.Id.FIRE_SLIME) ? true : false; //Same as above, only with fire slimes
+                        bool quicksilverFlag = false; //Same as above, only with quicksilver slimes. This remains blank and will be used if the "Quicksilver Rancher" mod is installed
+                        bool flag = puddleFlag || fireFlag; //If the slime is a puddle or a fire slime, the this variable will be true, else it will be false
+
+                        if(IsModLoaded("quicksilver_rancher") || IsModLoaded("frostys_quicksilver_rancher")) //If the "Quicksilver Rancher" mod is installed, then the code should conside quicksilver slimes
+                        {
+                            quicksilverFlag = (slime == Identifiable.Id.QUICKSILVER_SLIME) ? true : false; //Ternary Operator: If the id is the id of a quicksilver slime then return true, else false
+                            flag = flag || quicksilverFlag; //If the slime is a puddle or a fire or a quicksilver slime, the this variable will be true, else it will be false 
+                        }
+
+                        Console.Log("   Flag is: " + flag);
+                        if (flag) //If the slime is a puddle or fire or quicksilver slime then continue below
+                        {
+                            if(puddleFlag)// If the id (the slime variable) is a puddle slime then assign the variable "idPlort" the puddle plort id
+                            {
+                                idPlort = Identifiable.Id.PUDDLE_PLORT;
+                            }
+
+                            if (fireFlag)// If the id (the slime variable) is a fire slime then assign the variable "idPlort" the fire plort id
+                            {
+                                idPlort = Identifiable.Id.FIRE_PLORT;
+                            }
+
+                            if (quicksilverFlag)// If the id (the slime variable) is a quicksilver slime then assign the variable "idPlort" the quicksilver plort id
+                            {
+                                idPlort = Identifiable.Id.QUICKSILVER_PLORT;
+                            }
+
+                        }*/
+
+                        Console.Log("   idPlort is: " + idPlort);
+
+                        Console.Log("   Filter did a great job");
+                        //EconomyDirector.ValueMap valueMap = (flag) ? valueMaps.FindById(idPlort, true) : valueMaps.FindById(slime, false); //Read below
+
+                        EconomyDirector.ValueMap valueMap;
+                        if (!(slime == Identifiable.Id.PUDDLE_SLIME) && !(slime == Identifiable.Id.FIRE_SLIME))
+                        {
+                            valueMap = valueMaps.FindById(slime); //Read below
+                        }
+                        else
+                        {
+                            valueMap = valueMaps.FindById(slime.ToString());
+                        }
+
+                        //If the slime is a puddle/fire/quicksilver slime then add the plort id, else just add the id of the slime (true/false argumeter tells the programm if it's a puddle/fire/quicksilver slime or regular slimes, except tarrs and lucky slimes)
+                        //We are getting the informations of the plort of the correct slime (to make normal prices)
+                        //But if we find nothing then return null
+                        Console.Log("   valueMap not found...");
+                        if (valueMap == null)
+                            continue;
+                        Console.Log("   ...or maybe yes? Now registering slimes");
+
+                        if (slime == Identifiable.Id.SABER_SLIME) //If the slime is a Saber Slime
+                        {
+                            PlortRegistry.RegisterPlort(slime, valueMap.value * 50, valueMap.fullSaturation); //Then register the slime (make so that you can sell it) for 20 times the original price more.
+                            continue;
+                        }
+                        else if (slime == Identifiable.Id.GOLD_SLIME) //If the slime is a Gold Slime
+                        {
+                            PlortRegistry.RegisterPlort(slime, valueMap.value * 8, valueMap.fullSaturation); //Then register the slime (make so that you can sell it) for 8 times the original price more.
+                            continue;
+                        }
+
+                        PlortRegistry.RegisterPlort(slime, valueMap.value * 4, valueMap.fullSaturation); //Else, if the slime is a regular slime, then register the slime (make so that you can sell it) for 4 times the original price more.
+                        DroneRegistry.RegisterBasicTarget(slime); //And make so that the drones can grab slimes
+
+                        Console.Log("Slime: " + slime + " registred perfectly");
+                    }
+                }
+                catch (Exception e) //But if an error was found
+                {
+                    Console.Log("Slimes and Market mod error (Vanilla): " + e.Message); //Then log the error into the SRML.log file
+                }
+
+                try
+                {
+                    foreach (ModdedSellable item in ModdedSellable.moddedItemsToSell)
+                    {
+                        PlortRegistry.RegisterPlort(item.itemToSell, item.price, item.saturation);
+
+                        if (item.droneTake)
+                            DroneRegistry.RegisterBasicTarget(item.itemToSell);
+
+                        ModdedThings.modsRegistred.Add(item.nameOfMod);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Log("Slimes and Market mod error (Modded): " + e.Message); //Then log the error into the SRML.log file
+                }
+
+
+            }
+
+        }
     }
 }
